17c3674a116a69b9176dd11bc1d307b8447ea6c5
Diki Siswanto <dickyachmady@gmail.com>
2022-07-21 01:46:14 +0800

    chore: delete test folder

--- src/__test__/Header.test.js
+++
@@ -1,8 +0,0 @@
-import Header from "@components/Header";
-import { render } from "@testing-library/react";
-import * as React from "react";
-
-/* eslint-disable no-undef */
-it('Should render header with heading role and correct text', () => {
-  render(<Header />);
-});
